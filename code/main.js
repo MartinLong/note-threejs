f0b1ec85c6b4d12c4c967e17aa561c8e6980b2d9
longbin.lb <longbin.lb@C02V29Z7G8WN.local>
2020-04-30 00:17:39 +0800

    第一章的学习记录

---
+++ code/main.js
@@ -0,0 +1,89 @@
+function init() {
+    var scene = new THREE.Scene();//场景（容器，保存、跟踪需渲染的物体和光源）
+    var camara = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var stats = initStats();
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(new THREE.Color(0x000000));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+
+    var axes = new THREE.AxesHelper(20);
+    scene.add(axes);
+
+    var planeGeometry = new THREE.PlaneGeometry(60, 20);
+    var planeMaterial = new THREE.MeshLambertMaterial({
+        color: 0xFFFFFF
+    });
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.set(10, 0, 0);
+    plane.receiveShadow = true;
+    scene.add(plane);
+
+    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+    var cubeMaterial = new THREE.MeshLambertMaterial({
+        color: 0xFF0000
+    });
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.set(-4, 4, 4);
+    cube.castShadow = true;
+    scene.add(cube);
+
+    var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+    var sphereMeterial = new THREE.MeshLambertMaterial({
+        color: 0x7777FF
+    });
+    var sphere = new THREE.Mesh(sphereGeometry, sphereMeterial);
+    sphere.position.set(20, 4, 2);
+    sphere.castShadow = true;
+    scene.add(sphere);
+
+    var spotLight = new THREE.SpotLight(0xFFFFFF);
+    spotLight.position.set(-40, 40, -15);
+    spotLight.castShadow = true;
+    spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+    spotLight.shadow.camera.far = 130;
+    spotLight.shadow.camera.near = 40;
+    scene.add(spotLight);
+
+    camara.position.set(-20, 30, 30);
+    camara.lookAt(scene.position);
+
+    document.getElementById("webgl-output").appendChild(renderer.domElement);
+
+    var step = 0;
+    
+    var controls = new function () {
+        this.rotationSpeed = 0.02;
+        this.bouncingSpeed = 0.04;
+    }
+
+    function renderScene() {
+        stats.update();
+
+        cube.rotation.x += controls.rotationSpeed;
+        cube.rotation.y += controls.rotationSpeed;
+        cube.rotation.z += controls.rotationSpeed;
+
+        step += controls.bouncingSpeed;
+        sphere.position.x = 20 + 10 * (Math.cos(step));
+        sphere.position.y = 4 + 10 * Math.abs(Math.sin(step));
+
+        requestAnimationFrame(renderScene);
+        renderer.render(scene, camara);
+    }
+    var gui = new dat.GUI();
+    gui.add(controls, 'rotationSpeed', 0, 0.5);
+    gui.add(controls, 'bouncingSpeed', 0, 0.5);
+
+    renderScene();
+};
+
+function initStats(type) {
+    var panelType = (typeof type !== 'undefined' && type) && (!isNaN(type)) ? parseInt(type) : 0;
+    var stats = new Stats();
+    stats.showPanel(panelType); // 0:fps,1:ms,2:mb,3+:custom
+    document.body.appendChild(stats.dom);
+    return stats;
+}
--- doc/Chapter1.md
+++ doc/Chapter1.md
@@ -11,7 +11,55 @@
 3.Mac系统
 >open -a "Google Chrome" --args --disable-web-security
 
+### vscode three.js 智能提示
+1.安装node.js，使用npm
 
+2.执行命令安装typings 
+>npm install -g typings
+
+3.项目根目录下执行命令安装three
+>npm install three
+
+4.新建一个js文件，即可使用代码提示了
+
+### 显示必要的类或元素
+1.Scene 场景，容器用于保存、跟踪需渲染物体和光源
+2.Camera 摄像机，决定场景能在场景看到什么
+3.Renderer 渲染器，使用电脑显卡来渲染场景
+
+### 渲染对象组成
+1.Geometry 定义大小
+2.Material 定义外观
+3.Mesh 网格对象
+
+### 光源
+1.spotLight 点光源
+2.
+
+### 阴影
+1.默认情况不会渲染阴影，增加阴影效果需做以下设置
+>renderer.shadowMap.Enabled = true; //允许渲染阴影
+>plane.receiveShadow = true; //接受阴影的物体
+>cube.castShadow = true; //投射阴影的物体
+
+2.阴影需要耗费大量的计算资源
+
+### 材质
+1.MeshBasicMaterial 不会对光源有任何反应，只会用指定颜色来渲染物体
+2.MeshLambertMaterial 
+3.MeshPhysicalMaterial
+4.MeshStandardMaterial
+5.MeshPhongMaterial 已弃用
+
+### 场景定时刷新函数的优缺点
+1.setInterval 缺点是不管浏览器发生什么（如浏览其他网页）都会每隔x毫秒执行一次；而且并没有与屏幕的刷新同步。导致较高的cpu使用率和性能不良。
+2.requestAnimationFrame 函数为为稳定连续的渲染场景提供了良好解决方案。在这个方法的回调函数里完成一帧的绘制操作即可，剩下的交给浏览器。
+
+### 打印当前帧率的库
+stats.js
+
+### 方便调试的第三方GUI库
+dat.GUI
 
 
 
